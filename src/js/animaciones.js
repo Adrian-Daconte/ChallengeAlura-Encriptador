248426a228d29d2d8ce712eb4e4edf2ddb69355f
Adrian Daconte <daconteadrian2018@gmail.com>
2024-08-07 19:57:08 -0500

    Challenge Encriptador

---
+++ src/js/animaciones.js
@@ -0,0 +1,25 @@
+import { verificación } from "./main.js";
+
+export const popup = document.querySelector(".div-popup");
+export const fondoPopup = document.querySelector(".fondo-popup");
+export function cerrarPopUp() {
+  
+  const cerrar = document.querySelector(".cerrar-popup");
+  cerrar.addEventListener("click", () => {
+    popup.classList.remove("div-popup");
+    popup.classList.add("non");
+    fondoPopup.classList.remove("fondo_popup");
+    fondoPopup.classList.add("non");
+  });
+}
+
+export function abrirPopUp() {
+   
+   popup.classList.add('div-popup');
+   popup.classList.remove('non');
+   fondoPopup.classList.add('fondo_popup');
+   fondoPopup.classList.remove('non');
+    
+} 
+
+
---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+import "../../scss/styles.scss";
+
+import {cerrarPopUp,abrirPopUp } from "./animaciones.js";
+
+const btnEncriptar = document.querySelector("#botonEncriptar");
+const expresionRegular = /^[a-z\s]+$/;
+export let verificación;
+
+
+
+function revisarTexto() {
+  btnEncriptar.addEventListener("click", () => {
+    let textoRecibido = document.querySelector("#area__input").value;
+    let verificado = undefined;
+
+    if (expresionRegular.test(textoRecibido)) {
+     
+     
+
+    } else {
+     
+      abrirPopUp();
+    }
+
+    return verificación;
+  });
+}
+
+
+revisarTexto();
+cerrarPopUp();
+
+//funcion para encriptar el texto
+function encriptar() {
+  btnEncriptar.addEventListener("click", () => {
+    let textoRecibido = document.querySelector("#area__input").value;
+
+    textoRecibido = textoRecibido.split("");
+    console.log(textoRecibido);
+  });
+}
+encriptar();
+
+//funcion para  revisar condiciones del texto
